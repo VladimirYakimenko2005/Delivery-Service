3fd62da1a1b07828f9ecc6394cc6d936d05ad2ab
Владимир Якименко <thevladimiryakimenko@gmail.com>
2024-05-19 16:06:09 +0300

    v.3

--- delivery_service.py
+++ delivery_service.py
@@ -1,26 +1,17 @@
-def delivery_service(robots: list, limit: float):
-    if min(robots) > limit / 2 or len(robots) == 1:
-        return len(robots)
-    else:
-        robots.sort(reverse=True)
-        count_platforms = 0
-        count_robots = 0
-        last = -1
-        penult = last - 1
-        while True:
-            if (len(robots) > count_robots + 1
-                    and robots[last] + robots[penult] <= limit):
-                last -= 2
-                penult -= 2
-                count_platforms += 1
-                count_robots += 2
-            else:
-                count_platforms += len(robots[last::-1])
-                break
-    return count_platforms
+def count_platforms(robots, limit):
+    robots.sort()
+    left_pointer = 0
+    right_pointer = len(robots) - 1
+    count = 0
+    while left_pointer <= right_pointer:
+        count += 1
+        if robots[left_pointer] + robots[right_pointer] <= limit:
+            left_pointer += 1
+        right_pointer -= 1
+    return count
 
 
 robots = list(map(float, input().split()))
 limit = float(input())
 
-print(delivery_service(robots, limit))
+print(count_platforms(robots, limit))
